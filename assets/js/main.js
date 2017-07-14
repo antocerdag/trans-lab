342dda1bedf94f1db42460b974726aa957f32bed
Antonia Cerda <antoniacerdag@gmail.com>
2017-07-14 09:45:39 -0400

    llamando API

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 		var password = $("#password").val();
 		if(!(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(localStorage.email))){
 			alert("Por favor ingresa un correo electrónico válido");
-		} else if((password == "")|| (password < 8) ){
+		} else if((password == "")|| (password.length < 8) || (password.length > 8) ){
 			alert("Por favor ingresa una contraseña, debe contener 8 números")
 		} else{
 			window.location.href = "home.html";
@@ -18,31 +18,43 @@ $(document).ready(function() {
 		$(".email-local").append(localstorage.email);
 
 	});
-	
+	// FUNCION PARA IMPRIMIR NUMEROS DE TARJETAS
 	$(".agregar-tarjeta").click(function(event) {
-		var numerotarjeta = $("#numerotarjeta").val();
-
-		$(".numero").append("<li>"+numerotarjeta+"</li>");
-		$('input:text').focus(function(){
+		localStorage.numerotarjeta = $("#numerotarjeta").val();
+		if((localStorage.numerotarjeta.length < 8 ) || (localStorage.numerotarjeta == "")|| (localStorage.numerotarjeta.length > 8 )){
+			alert("Ingrese un numero de tarjeta Bip! válido, deben ser 8 números");
+			$('input:text').focus(function(){
         	$(this).val('');
-    	});
+    		});
+		}else{
+			$(".numero").append("<li>"+localStorage.numerotarjeta+"</li>");
+			$('input:text').focus(function(){
+        	$(this).val('');
+    		});
+    		$(".numeroguardado").append(localStorage.numerotarjeta);
+		}
+		
 	});
-
-	$.ajax({
-		url: '/path/to/file',
-		type: 'default GET (Other values: POST)',
-		dataType: 'default: Intelligent Guess (Other values: xml, json, script, or html)',
-		data: {param1: 'value1'},
-	})
-	.done(function() {
-		console.log("success");
-	})
-	.fail(function() {
-		console.log("error");
-	})
-	.always(function() {
-		console.log("complete");
+	//ver saldo
+	$(".ver-saldo").click(function(event) {
+		var id = $(".inputsaldo").val();
+		$.ajax({
+			url: 'http://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip='+id,
+			type: 'GET',
+			dataType: 'json',
+		})
+		.done(function(response) {
+			console.log("success");
+			console.log(response);
+		})
+		.fail(function() {
+			console.log("error");
+		})
+		.always(function() {
+			console.log("complete");
+		});
 	});
+	
 	
 
 
