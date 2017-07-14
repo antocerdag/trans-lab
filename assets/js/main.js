cc831ead4cacb626d707127e7f23d322f4f6ac37
Antonia Cerda <antoniacerdag@gmail.com>
2017-07-14 11:32:46 -0400

    api no llama id

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,27 +37,26 @@ $(document).ready(function() {
 	});
 	//ver saldo
 	$(".ver-saldo").click(function(event) {
-		var id = $(".inputsaldo").val();
+		var id = ($(".inputsaldo").val());
 		var value = $(".numeroguardado").val();
-		if(id == ""){
-			alert("Ingresa un numero de tarjeta válido");
-		} else{
-
-		 $.ajax({
-            url : 'api/v1/solicitudes.json',
-            type : 'GET',
-            dataType: 'json',
-            data : {'bip' : numTarjeta},
-            beforeSend: function(){
-                $('#modalresultado').modal('show');
-            },
-        success : function(data){
-        	$(".datos-tarjeta").append('<b>Saldo tarjeta bip: </b>'+data.saldoTarjeta+'<br>'+'<b>Fecha saldo: </b>'+data.fechaSaldo+'<br>'+'<b>ID Tarjeta: </b>'+data.id+'<br>'+'<b>Estado contrato: </b>'+data.estadoContrato+'<br>');
-        }
-		
+		if(id.length = 8){
+			$.ajax({
+	            url:'http://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip='+id+'',
+	            type: 'GET',
+	            datatype: 'json',
+	        	})
+	        .done(function(response){
+	            console.log(response.saldoTarjeta);
+	            $('#modalresultado').modal('show');
+	            $("#datos-tarjeta").append("<b>Saldo tarjeta bip: </b>"+response.saldoTarjeta+"<br><b>Fecha saldo: </b>"+response.fechaSaldo+"<br><b>ID Tarjeta: </b>"+response.id+"<br><b>Estado contrato: </b>"+response.estadoContrato+"<br>");
+	        })
+		    .fail(function(){
+		        console.log("error");
+		    })
+			
+		}
+	
 	});
-	
-	
 
 
 	$('.collapsible').collapsible();
